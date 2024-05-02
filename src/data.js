88475ecae5e49e784442eae47bfc01418e0f6dc1
CDay98 <102760296+CDay98@users.noreply.github.com>
2024-05-02 12:05:51 -0400

    init commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Python Discord Bot",
+    subtitle: "Python and Discord API",
+    description:
+      "PlaceHolderProject",
+    image: "./ScreenRecording2024-05-02at11.14.33AM-ezgif.com-effects.gif",
+    link: "",
+  },
+  {
+    title: "MusicBotWet",
+    subtitle: "Html, CSS, and Javascript",
+    description:
+      "PlaceHolderProject",
+    image: "./project-2.gif",
+    link: "",
+  },
+  {
+    title: "PlaceHolderProject",
+    subtitle: "PlaceHolderProject",
+    description:
+      "PlaceHolderProject",
+    image: "./project-3.gif",
+    link: "",
+  },
+  {
+    title: "PlaceHolderProject",
+    subtitle: "PlaceHolderProject",
+    description:
+      "PlaceHolderProject",
+    image: "./project-4.gif",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Get Testimonials",
+    image: "https://randomuser.me/api/portraits/men/3.jpg",
+    name: "Place Holder",
+    company: "Place Holder",
+  },
+  {
+    quote:
+      "Get Testimonials",
+    image: "https://randomuser.me/api/portraits/men/54.jpg",
+    name: "Place Holder",
+    company: "Place Holder",
+  },
+];
+
+export const skills = [
+  "Python",
+  "Javascript",
+  "React Native",
+  "SQL",
+  "APIs",
+  "Oracle Database",
+];
