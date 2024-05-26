aa5f1138d9f39495a1ea40a3e6f85053111d7c35
CDay98 <102760296+CDay98@users.noreply.github.com>
2024-05-25 23:17:02 -0400

    updated description

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,14 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container mx-auto mt-20 flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Colin.
           </h1>
-          <h1 className="title-font sm:text-2xl text-1xl mb-4 font-medium text-white">
-            *Write description*
+          <h1 className="title-font sm:text-2xl text-1xl mb-4 font-xs text-grey">
+            I'm a dedicated back end software engineer specializing in enterprise systems and custom development. With a passion for crafting robust, scalable solutions, I thrive on transforming complex requirements into efficient, high-performance applications. Let's connect and create something extraordinary together.
           </h1>
-          
           <div className="flex justify-center">
             <a
               href="#contact"
